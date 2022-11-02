e681550ae8236753e7bce53778ceb81a6ccd12c4
TDWHayster <81854183+TDWHayster@users.noreply.github.com>
2022-11-02 00:14:40 +0000

    Take 2

---
+++ Aelthenoa/SubModule.cs
@@ -0,0 +1,28 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Aelthenoa
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+        }
+
+        protected override void OnApplicationTick(float dt)
+        {
+            base.OnApplicationTick(dt);
+        }
+
+        public override void OnAfterGameInitializationFinished(Game game, object starterObject)
+        {
+            base.OnAfterGameInitializationFinished(game, starterObject);
+        }
+
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            base.OnMissionBehaviorInitialize(mission);
+        }
+    }
+}
